aa7e52723961ae17cd1fd47ca41f63c8d190a848
perry <samesense@gmail.com>
2026-04-08 22:34:25 -0400

    Add maul.

--- lua/maul/registry.lua
+++ lua/maul/registry.lua
@@ -1,4 +1,8 @@
 return {
+  ["maul"] = {
+    flavour = "mocha",
+    palette = require("maul.palettes.maul"),
+  },
   ["maulCircle"] = {
     flavour = "mocha",
     palette = require("maul.palettes.maulCircle"),
